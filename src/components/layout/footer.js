719744e6cb0f6cf252df473529737eb5047ccc97
JosipSudar <josip.sudar@gmail.com>
2023-03-10 15:05:17 +0100

    John Doe

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,32 +1,51 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
+    <footer className=" container-sm p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
       <div className="sm:flex sm:items-center sm:justify-between">
-        <a href="" className="flex items-center mb-4 sm:mb-0">
+        <Link
+          href="/HeaderComponent"
+          className="flex items-center mb-4 sm:mb-0"
+        >
           <img src="./slike/cart.png" className="h-8 mr-3" alt="Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Web-Shop</span>
-        </a>
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            Web-Shop
+          </span>
+        </Link>
         <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
           <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
+            <Link href="/about" className="mr-4 hover:underline md:mr-6 ">
+              About
+            </Link>
           </li>
           <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+            <Link href="/privacy" className="mr-4 hover:underline md:mr-6">
+              Privacy Policy
+            </Link>
           </li>
           <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6 ">Licensing</a>
+            <Link href="/licensing" className="mr-4 hover:underline md:mr-6 ">
+              Licensing
+            </Link>
           </li>
           <li>
-            <a href="#" className="hover:underline">Contact</a>
+            <Link href="/contact" className="hover:underline">
+              Contact
+            </Link>
           </li>
         </ul>
       </div>
       <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-      <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">©<a href="" className="hover:underline">Web-Shop</a>. All Rights Reserved.
+      <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        ©
+        <Link href="/HeaderComponent" className="hover:underline">
+          Web-Shop
+        </Link>
+        . All Rights Reserved.
       </span>
     </footer>
   );
-}
-export default Footer
+};
+export default Footer;
